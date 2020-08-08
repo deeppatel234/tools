1321a6bb61c5bfb149fc2301ae5450e38443d49e
Deep Patel <deep.patel@deep.patel>
2020-08-08 20:58:08 +0530

    Updates

---
+++ precache-manifest.ba3c17ae569911634eb2eae4aea6a717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9b2e03a45f1e0f57fffd213e338c2d",
+    "url": "/tools/index.html"
+  },
+  {
+    "revision": "3599e304c35b82c17251",
+    "url": "/tools/static/css/main.9e93771d.chunk.css"
+  },
+  {
+    "revision": "25590dfb437df5c8359d",
+    "url": "/tools/static/js/2.ee7a404a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tools/static/js/2.ee7a404a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3599e304c35b82c17251",
+    "url": "/tools/static/js/main.a98b12c1.chunk.js"
+  },
+  {
+    "revision": "b1518d91fd0a1eb7615a",
+    "url": "/tools/static/js/runtime-main.07a40949.js"
+  }
+]);
