50a79a0f984cdd3fb9bbc05a99e314fbc2fb4ff0
Deep Patel <deeppatel234@gmail.com>
2020-08-15 20:01:49 +0530

    add search in tab

--- src/containers/JSONView/index.js
+++ src/containers/JSONView/index.js
@@ -12,12 +12,13 @@ import { keyBy } from "../../utils";
 import "./index.scss";
 
 const JSONView = () => {
-  const [ jsonValue, setJsonValue ] = useState("");
-  const [ tabs, setTabs ] = useState({});
-  const [ selectedTabId, setSelectedTabId ] = useState(null);
+  const [jsonValue, setJsonValue] = useState("");
+  const [tabs, setTabs] = useState({});
+  const [selectedTabId, setSelectedTabId] = useState(null);
+  const [searchText, setSearch] = useState("");
 
   const fetchData = () => {
-    JSONTab.getAllTabs().then(data => {
+    JSONTab.getAllTabs().then((data) => {
       if (Array.isArray(data) && data.length) {
         setTabs(keyBy(data, "id"));
       } else {
@@ -28,7 +29,7 @@ const JSONView = () => {
 
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (!tabs[selectedTabId]) {
@@ -50,38 +51,51 @@ const JSONView = () => {
   };
 
   const onClickAddTab = () => {
-    JSONTab.add(JSONTab.getInitialData()).then(() => {
-      fetchData();
-    }).catch((err) => {
-      console.log('error', err);
-    });
+    JSONTab.add(JSONTab.getInitialData())
+      .then(() => {
+        fetchData();
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
   };
 
   const onChangeTitle = (newHeader) => {
-    JSONTab.changeTitle(selectedTabId, newHeader).then(() => {
-      setTabs({ ...tabs, [selectedTabId]: {
-        ...tabs[selectedTabId],
-        title: newHeader
-      }});
-    }).catch((err) => {
-      console.log('error', err);
-    });
+    JSONTab.changeTitle(selectedTabId, newHeader)
+      .then(() => {
+        setTabs({
+          ...tabs,
+          [selectedTabId]: {
+            ...tabs[selectedTabId],
+            title: newHeader,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
   };
 
   const onClickDeleteTab = (id) => {
-    JSONTab.delete(id).then(() => {
-      setTabs(oldTabs => {
-        const copyTabs = { ...oldTabs };
-        delete copyTabs[id];
-        return copyTabs;
+    JSONTab.delete(id)
+      .then(() => {
+        setTabs((oldTabs) => {
+          const copyTabs = { ...oldTabs };
+          delete copyTabs[id];
+          return copyTabs;
+        });
+      })
+      .catch((err) => {
+        console.log("error", err);
       });
-    }).catch((err) => {
-      console.log('error', err);
-    });
   };
 
-  const onChangeTab = id => {
+  const onChangeTab = (id) => {
     setSelectedTabId(id);
+  };
+
+  const onSearch = (value) => {
+    setSearch(value);
   };
 
   const tabList = Object.values(tabs);
@@ -92,52 +106,60 @@ const JSONView = () => {
         <div className="tab-header">
           <h4 className="title">Tabs</h4>
           <Tippy content="Add a new tab">
-            <Plus className="add-tab" onClick={onClickAddTab}/>
+            <Plus className="add-tab" onClick={onClickAddTab} />
           </Tippy>
         </div>
+        <div className="tab-search">
+          <input
+            value={searchText}
+            onChange={(e) => onSearch(e.target.value)}
+            className="search-input"
+            placeholder="Search..."
+          />
+        </div>
         <div className="tab-list">
-          {
-            tabList.map(({ id, title }) => (
+          {tabList.map(({ id, title }) => {
+            if (searchText && !title.toLowerCase().includes(searchText.toLowerCase())) {
+              return null;
+            }
+
+            return (
               <div
                 key={id}
                 className={classnames("tab", {
-                  "active": selectedTabId === id,
+                  active: selectedTabId === id,
                 })}
                 onClick={() => onChangeTab(id)}
               >
                 <span className="tab-name">{title}</span>
-                {
-                  tabList.length > 1 ? (
-                    <Tippy content="Delete tab">
-                      <Cross
-                        className="delete-tab"
-                        onClick={() => onClickDeleteTab(id)}
-                      />
-                    </Tippy>
-                  ) : null
-                }
+                {tabList.length > 1 ? (
+                  <Tippy content="Delete tab">
+                    <Cross
+                      className="delete-tab"
+                      onClick={() => onClickDeleteTab(id)}
+                    />
+                  </Tippy>
+                ) : null}
               </div>
-            ))
-          }
+            );
+          })}
         </div>
       </div>
       <div className="json-editor">
-        {
-          (selectedTabId && tabs[selectedTabId]) ? (
-            <Editor
-              key={tabs[selectedTabId].id}
-              id={tabs[selectedTabId].id}
-              title={tabs[selectedTabId].title}
-              value={jsonValue}
-              jsonEditor
-              jsonModeEnabled
-              enableJSONLint
-              headerEditable
-              onValueChange={onChangeJson}
-              onChangeHeader={onChangeTitle}
-            />
-          ) : null
-        }
+        {selectedTabId && tabs[selectedTabId] ? (
+          <Editor
+            key={tabs[selectedTabId].id}
+            id={tabs[selectedTabId].id}
+            title={tabs[selectedTabId].title}
+            value={jsonValue}
+            jsonEditor
+            jsonModeEnabled
+            enableJSONLint
+            headerEditable
+            onValueChange={onChangeJson}
+            onChangeHeader={onChangeTitle}
+          />
+        ) : null}
       </div>
     </section>
   );
